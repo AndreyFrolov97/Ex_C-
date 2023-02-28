80dfbeb22775d01c9c3aa466a8a3681dae4c0b93
Andrey <Frolovandr97@mail.ru>
2023-02-28 16:48:43 +0700

    нахождение третьего числа

---
+++ exm/ex013/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string numStr = Convert.ToString(num);
+int numDigit = numStr.Length;
+if (numDigit > 2) Console.WriteLine($"Третья цифра введённого числа: {numStr[2]}");
+else Console.WriteLine("третьей цифры нет");
