ece08f6167c81719efe852df92dd5b34552a91d5
Andrey <Frolovandr97@mail.ru>
2023-02-28 14:25:26 +0700

    Второе из трехзначного

---
+++ exm/ex010/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Write("Введите трехзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int numDigit = 3;
+
+ if (numDigit <= 3)
+{
+    Console.WriteLine("Ошибка. Введено некорректное число.");
+}
+else
+{
+    if (numDigit > 3)
+    {
+        num = num / Convert.ToInt32(Math.Pow(10, numDigit - 3)); 
+    }
+    num = num / 10 % 10;
+    Console.WriteLine($"Вторая цифра введённого числа: {num}");
+}
